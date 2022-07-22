a0baaa9229a9f45be4ffe6be5b906c7a085a48ef
VaishnawiDighore <vaishnawidighore.@gmail.com>
2022-07-22 20:18:17 +0530

    first commit

---
+++ src/Constants.js
@@ -0,0 +1,6 @@
+const emojis = ['😎', '🍀', '⏰', '🥣', '🌎', '💖', '🐵', '👋'];
+const pair_emojis = [...emojis, ...emojis];
+const initRevealState = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+export{emojis, pair_emojis, initRevealState};
+
